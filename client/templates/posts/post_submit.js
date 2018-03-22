65c24efd8e11e17a473b569c596096f73ba46d9b
shirleyrz <rongzou.shirley@gmail.com>
2018-03-22 03:13:20 -0700

    Use a method to submit the post.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,17 +1,14 @@
 Template.postSubmit.events({
     'submit form': function(e) {
         e.preventDefault();
-
         var post = {
             url: $(e.target).find('[name=url]').val(),
             title: $(e.target).find('[name=title]').val()
         };
-
         Meteor.call('postInsert', post, function(error, result) {
-            // display the error to the user and abort
+            // 显示错误信息并退出
             if (error)
                 return alert(error.reason);
-
             Router.go('postPage', {_id: result._id});
         });
     }
