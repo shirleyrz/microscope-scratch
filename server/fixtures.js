5481c0ec653cef7d3b866d91eb43e8c7d4e07f05
shirleyrz <rongzou.shirley@gmail.com>
2018-03-22 02:06:20 -0700

    remove insecure & restrict writes to posts.

--- server/fixtures.js
+++ server/fixtures.js
@@ -2,7 +2,7 @@ if (Posts.find().count() === 0) {
   Posts.insert({
     title: 'Introducing Telescope',
     url: 'http://sachagreif.com/introducing-telescope/',
-    author: "bob-smith",
+    author: 'bob-smith',
       flagged: false
 
   });
@@ -10,13 +10,14 @@ if (Posts.find().count() === 0) {
   Posts.insert({
     title: 'Meteor',
     url: 'http://meteor.com',
-      author: "bob-smith",
+      author: 'bob-smith',
       flagged: false
   });
 
   Posts.insert({
     title: 'The Meteor Book',
     url: 'http://themeteorbook.com',
+      author: 'bob-smith',
       flagged: false
   });
 }
