b4adcc8f32e50352d106cec0fe88958409867e50
shirleyrz <rongzou.shirley@gmail.com>
2018-03-22 17:44:14 -0700

    9-5 Validate post contents when editing

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,3 +1,16 @@
+Template.postEdit.onCreated(function() {
+    Session.set('postEditErrors', {});
+});
+
+Template.postEdit.helpers({
+    errorMessage: function(field) {
+        return Session.get('postEditErrors')[field];
+    },
+    errorClass: function (field) {
+        return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+    }
+});
+
 Template.postEdit.events({
     'submit form': function(e) {
         e.preventDefault();
@@ -9,6 +22,10 @@ Template.postEdit.events({
             title: $(e.target).find('[name=title]').val()
         }
 
+        var errors = validatePost(postProperties);
+        if (errors.title || errors.url)
+            return Session.set('postEditErrors', errors);
+
         Posts.update(currentPostId, {$set: postProperties}, function(error) {
             if (error) {
                 // display the error to the user
@@ -18,5 +35,14 @@ Template.postEdit.events({
             }
         });
     },
-    //...
+
+    'click .delete': function(e) {
+        e.preventDefault();
+
+        if (confirm("Delete this post?")) {
+            var currentPostId = this._id;
+            Posts.remove(currentPostId);
+            Router.go('postsList');
+        }
+    }
 });
