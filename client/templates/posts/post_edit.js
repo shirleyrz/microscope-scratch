35cb83e1749cda529a9d730d80350c2cfeaf57cc
shirleyrz <rongzou.shirley@gmail.com>
2018-03-22 17:25:15 -0700

    9-2 Actually use the error reporting.

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -12,20 +12,11 @@ Template.postEdit.events({
         Posts.update(currentPostId, {$set: postProperties}, function(error) {
             if (error) {
                 // display the error to the user
-                alert(error.reason);
+                throwError(error.reason);
             } else {
                 Router.go('postPage', {_id: currentPostId});
             }
         });
     },
-
-    'click .delete': function(e) {
-        e.preventDefault();
-
-        if (confirm("Delete this post?")) {
-            var currentPostId = this._id;
-            Posts.remove(currentPostId);
-            Router.go('postsList');
-        }
-    }
+    //...
 });
